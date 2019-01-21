aac39555eb7655528c05e6e3506fb9b0f0a5f89a
MysticBoy <mysticboy@live.com>
2019-01-21 23:14:18 +0800

    add support  windows service

--- MQTT.Chat/Program.cs
+++ MQTT.Chat/Program.cs
@@ -1,5 +1,9 @@
 ﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Hosting.WindowsServices;
+using MQTT.Chat.Extensions;
+using System;
+using System.Runtime.InteropServices;
 using System.Text;
 
 namespace MQTT.Chat
@@ -9,11 +13,21 @@ namespace MQTT.Chat
         public static void Main(string[] args)
         {
             Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
-            CreateWebHostBuilder(args).Build().Run();
+            var host = CreateWebHostBuilder(args).Build();
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows) && host.IsRunningAsAService())
+            {
+                System.IO.Directory.SetCurrentDirectory(AppContext.BaseDirectory);
+                host.RunAsService();
+            }
+            else
+            {
+                host.Run();
+            }
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+             .UseKestrel(options => options.ListenAnyIP(5000))
+             .UseStartup<Startup>();
     }
 }
